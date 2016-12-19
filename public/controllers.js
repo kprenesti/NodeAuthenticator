2d4b9b46ccef6d19f5e7c5b2871cb37adca59694
Krista Prenesti <kprenesti@gmail.com>
2016-12-19 12:34:44 -0500

    Updated functionality for signup

--- public/controllers.js
+++ public/controllers.js
@@ -1,41 +1,60 @@
 var app = angular.module('app');
-  app.controller('signUpController', function($http, userInfo){
+  app.controller('signUpController', function($http, userInfo, $state){
     var signup = this;
+    signup.error = "";
 
     signup.noFieldsBlank = function(username, firstName, lastName, password1, password2){
       if(!username || !firstName || !lastName || !password1 || !password2){
-        return signup.error = "Please fill in all fields."
-      }
-    };
-
-    signup. checkPassword = function(pw1, pw2){
-      if(pw1 === pw2){
-      signup.password = pw1;
+        signup.error = "Please fill in all fields."
+        return false;
       } else {
-          signup.error = "Error: Passwords Don't Match!";
-        }
-    };
-
-    signup.function checkEmail(email) {
-      var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-      if (!reg.test(email)) {
-        return signUp.error = "Error: Please enter a valid e-mail address.";
+        return true;
       }
-      return true;
-  };
-
-    signup.user = {
-      username: signup.username,
-      firstName: signup.firstName,
-      lastName: signup.lastName,
-      password: signup.password,
-      email: signup.email
     };
 
-    signup.submitUser = function(){
-      // signup.checkPassword(signUp.password1, signUp.password2);
-      // signup.checkEmail(signup.email);
-      console.log('The function is properly connected');
+    signup.checkPassword = function(pw1, pw2){
+      if(pw1 && pw2){
+        if(pw1 === pw2){
+          signup.password = pw1;
+          return true;
+        } else if(pw1 != pw2) {
+          signup.error = "Passwords don't match!"
+          return false;
+        }
+      } else if(!pw1 || !pw2) {
+        signup.error = "Please enter a valid password."
+        return false;
+      }
+    };
+
+    signup.checkFields = function(){
+      var fields = signup.noFieldsBlank(signup.username, signup.firstName, signup.lastName, signup.password1, signup.password2);
+      var pw;
+      if(fields){
+         pw = signup.checkPassword(signup.password1, signup.password2);
+      }
+      if(!fields || !pw){
+        $state.transitionTo($state.current, {}, {reload: false});
+      }
+      signup.user = {
+        username: signup.username,
+        firstName: signup.firstName,
+        lastName: signup.lastName,
+        password: signup.password,
+        email: signup.email
+      };
+      if(fields && pw){
+        $http.post('/users', JSON.stringify(signup.user)).then(function(user){
+
+          console.log(user);
+        }).catch(function(e){
+          console.log(e);
+        });
+        console.log(fields, pw);
+      }
+
+      // $state.transitionTo($state.current, {} ,{reload: true});
+
       // $http.post('/users', signup.user).then(function(user){
       //   userInfo.setUserData(user);
       //   console.log(user);
--- public/js/all.js
+++ public/js/all.js
@@ -55,43 +55,62 @@ angular.module('app', ['ui.router'])
 
 
 var app = angular.module('app');
-  app.controller('signUpController', function($http, userInfo){
+  app.controller('signUpController', function($http, userInfo, $state){
     var signup = this;
+    signup.error = "";
 
     signup.noFieldsBlank = function(username, firstName, lastName, password1, password2){
       if(!username || !firstName || !lastName || !password1 || !password2){
-        return signup.error = "Please fill in all fields."
+        signup.error = "Please fill in all fields."
+        return false;
+      } else {
+        return true;
       }
     };
 
-    signup. checkPassword = function(pw1, pw2){
-      if(pw1 === pw2){
-      signup.password = pw1;
-      } else {
-          signup.error = "Error: Passwords Don't Match!";
+    signup.checkPassword = function(pw1, pw2){
+      if(pw1 && pw2){
+        if(pw1 === pw2){
+          signup.password = pw1;
+          return true;
+        } else if(pw1 != pw2) {
+          signup.error = "Passwords don't match!"
+          return false;
         }
+      } else if(!pw1 || !pw2) {
+        signup.error = "Please enter a valid password."
+        return false;
+      }
     };
 
-  //   signup.function checkEmail(email) {
-  //     var reg = /^([A-z0-9_\-\.])+\@([A-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-  //     if (!reg.test(email)) {
-  //       return signUp.error = "Error: Please enter a valid e-mail address.";
-  //     }
-  //     return true;
-  // };
+    signup.checkFields = function(){
+      var fields = signup.noFieldsBlank(signup.username, signup.firstName, signup.lastName, signup.password1, signup.password2);
+      var pw;
+      if(fields){
+         pw = signup.checkPassword(signup.password1, signup.password2);
+      }
+      if(!fields || !pw){
+        $state.transitionTo($state.current, {}, {reload: false});
+      }
+      signup.user = {
+        username: signup.username,
+        firstName: signup.firstName,
+        lastName: signup.lastName,
+        password: signup.password,
+        email: signup.email
+      };
+      if(fields && pw){
+        $http.post('/users', JSON.stringify(signup.user)).then(function(user){
 
-    signup.user = {
-      username: signup.username,
-      firstName: signup.firstName,
-      lastName: signup.lastName,
-      password: signup.password,
-      email: signup.email
-    };
+          console.log(user);
+        }).catch(function(e){
+          console.log(e);
+        });
+        console.log(fields, pw);
+      }
 
-    signup.submitUser = function(){
-      // signup.checkPassword(signUp.password1, signUp.password2);
-      // signup.checkEmail(signup.email);
-      console.log('The function is properly connected');
+      // $state.transitionTo($state.current, {} ,{reload: true});
+
       // $http.post('/users', signup.user).then(function(user){
       //   userInfo.setUserData(user);
       //   console.log(user);
